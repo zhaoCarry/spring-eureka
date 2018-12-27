c62d28c63238f4bbe8615e294f67f29aa2bb48fb
zyf <zhao.yuanfang@trs.com.cn>
2018-12-27 16:21:55 +0800

    ccc

---
+++ src/main/java/com/example/springcloudeureka/SpringcloudEurekaApplication.java
@@ -0,0 +1,16 @@
+package com.example.springcloudeureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class SpringcloudEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudEurekaApplication.class, args);
+	}
+
+}
+
---
+++ src/main/java/com/example/springcloudeureka/mainTest.java
@@ -0,0 +1,8 @@
+package com.example.springcloudeureka;
+
+public class mainTest {
+
+    public static void main(String[] args) {
+
+    }
+}
